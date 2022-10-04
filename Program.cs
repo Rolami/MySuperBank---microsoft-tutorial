bc9455d5f9fac82c4cafe2c0adcd7e841adf7710
Obalanserad <Obalanserad@DESKTOP-N4OG1JP>
2022-10-04 09:19:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MySuperBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Gandalf", 100000);
+            Console.Write($"Account {account.Number} was created for {account.Owner} ");
+            Console.Write($"with {account.Balance} gold coins");
+
+
+            account.MakeWithdrawal(120, DateTime.Now, "Broom");
+            Console.WriteLine();
+            Console.WriteLine("Your balance is: " + account.Balance);
+
+
+        }
+    }
+}
